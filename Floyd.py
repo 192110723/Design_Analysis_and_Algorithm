91498c43b0a2ad12df87033ac98415ee5f4fe747
Amirtha Raveendran <113411317+192110723@users.noreply.github.com>
2024-03-06 14:00:17 +0530

    Floyd.py

--- Floyd.py
+++ Floyd.py
@@ -1,19 +1,29 @@
-def floyd_algorithm(graph):
-    n = len(graph)
-    dist = [row[:] for row in graph]
-    for k in range(n):
-        for i in range(n):
-            for j in range(n):
-                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+nV = 4
+INF = 999
 
-    return dist
-graph = [
-    [0, 5, float('inf'), 10],
-    [float('inf'), 0, 3, float('inf')],
-    [float('inf'), float('inf'), 0, 1],
-    [float('inf'), float('inf'), float('inf'), 0]
-]
+def floyd_warshall(G):
+    distance = G
 
-result = floyd_algorithm(graph)
-for row in result:
-    print(row)
+    for k in range(nV):
+        for i in range(nV):
+            for j in range(nV):
+                distance[i][j] = min(distance[i][j], distance[i][k] + distance[k][j])
+    print_solution(distance)
+
+def print_solution(distance):
+    for i in range(nV):
+        for j in range(nV):
+            if(distance[i][j] == INF):
+                print("INF", end=" ")
+                
+            else:
+                print(distance[i][j], end="  ")
+
+        print(" ")
+
+
+G = [[0, 3, INF, 5],
+         [2, 0, INF, 4],
+         [INF, 1, 0, INF],
+         [INF, INF, 2, 0]]
+floyd_warshall(G)
